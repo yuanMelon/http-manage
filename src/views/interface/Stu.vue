39b1644a6cb78c312332d5730b7caae3f3e0ac9d
unknown <11019348+useranna@user.noreply.gitee.com>
2023-08-24 00:43:45 +0800

    增加vite代理,处理跨域问题

--- src/views/interface/Stu.vue
+++ src/views/interface/Stu.vue
@@ -1,7 +1,7 @@
 /** * Query * @ author: yourName * @ data: 2023-08-19 13:43 */
 <script setup lang="ts">
 import { ref } from 'vue'
-import {toRefs,computed, reactive} from 'vue'
+import { toRefs, computed, reactive } from 'vue'
 
 import type { UnwrapRef } from 'vue'
 // import { useInterfaceStore } from '@/stores/useInterface'
@@ -45,16 +45,27 @@ let query = ref({
   name: 'string',
   required: 'string'
 })
-const emit = defineEmits(['query','child-click'])
+const emit = defineEmits(['query', 'child-click'])
 //提交组件数据
 const saveQueryRow = () => {
-    const newObj = JSON.parse(JSON.stringify(query.value))
-    emit('child-click',newObj)
+  const newObj = JSON.parse(JSON.stringify(query.value))
+  emit('child-click', newObj)
+}
+
+
+//导入axios模块
+import axios from 'axios';
+//es6异步方法 async await
+const getMusic = async function () {
+  //设置path,path会被vite的代理服务器拦截
+  const result = await axios.get('/163?id=22676167.mp3')
+  console.log(result)
 }
 </script>
 <template>
   <div class="">
     <!-- 表单由两个部分组成, -->
+    <button @click="getMusic">clickme</button>
     <a-form :layout="formState.layout" :model="formState">
       <a-form-item label="">
         <a-input v-model:value="query.name" placeholder="参数名称" />
--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,21 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    
+  },
+  server: {
+    //http://music.163.com/song/media/outer/url?id=501220770.mp3
+    port: 9999,
+    host: '0.0.0.0',
+    proxy:{
+      '/163':{
+        target:'http://music.163.com/song/media/outer/url',
+        secure:false,
+        changeOrigin:true,
+        rewrite:(path)=>path.replace(/^\/163/,'')
+
+      }
     }
   }
 })
